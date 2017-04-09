13e88845768298b6046dad422ee4b42eb7df8f89
Dale Campbell <oshuma@gmail.com>
2017-04-08 23:23:04 -0400

    Select new group on create.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,7 +14,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.build(group_params)
 
     if @group.save
-      redirect_to :back, notice: 'Group added.'
+      redirect_to new_comic_path(group_id: @group.id), notice: 'Group added.'
     else
       redirect_to :back, alert: 'There was a problem adding that group.'
     end
