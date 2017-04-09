8e505588c3050d6a151bca282a0f91e386559145
Dale Campbell <oshuma@gmail.com>
2017-04-08 21:54:12 -0400

    Updating deployed ruby version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ append :linked_dirs, "log", "public/system"
 
 # rbenv
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.2'
+set :rbenv_ruby, '2.4.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
