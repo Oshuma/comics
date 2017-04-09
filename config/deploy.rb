8e06bd64006b77ba15e291f13347ac1545510c64
Dale Campbell <oshuma@gmail.com>
2017-04-08 21:06:26 -0400

    Updating deploy shit.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,37 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock "3.8.0"
 
-set :application, 'comics'
-set :repo_url, 'ssh://twono.de:2200/home/repos/comics.git'
+set :application, "comics"
+set :repo_url, "https://github.com/Oshuma/comics.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/apps/twono.de/comics'
+set :deploy_to, "/apps/twono.de/comics"
 
-# Default value for :scm is :git
-# set :scm, :git
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public/system')
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
 
 # rbenv
 set :rbenv_type, :user
@@ -44,15 +41,3 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 # Passenger
 set :passenger_restart_with_touch, true
-
-
-namespace :deploy do
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-end
