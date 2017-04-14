51d2bca127367bc78a5c21bd80c2f213fe8ff8a3
Dale Campbell <oshuma@gmail.com>
2017-04-14 02:10:38 -0400

    Wrap Dropzone JS in a check so it doesn't try to attach on other pages.

--- app/assets/javascripts/comics.js
+++ app/assets/javascripts/comics.js
@@ -3,92 +3,95 @@
 
 $(document).on('turbolinks:load', function() {
 
-  if ($('#select_group').val() !== '') {
-    enableUploadButtons();
-  }
-
-  $('#select_group').on('change', function() {
-    if ($(this).val() === '') {
-      disableUploadButtons();
-    } else {
+  // HACK: We have to wrap the Dropzone shit in this check, so it won't try to attach on other pages.
+  if ($('#comic_upload').length) {
+    if ($('#select_group').val() !== '') {
       enableUploadButtons();
     }
-  });
 
-  // Get the template HTML and remove it from the doument
-  var previewNode = $('#template');
-  previewNode.removeAttr('id');
-  var previewTemplate = previewNode.parent().html();
-  previewNode.remove();
-
-  var comicUploadDropzone = new Dropzone('#comic_upload', {
-    paramName: 'comic',
-    thumbnailWidth: 80,
-    thumbnailHeight: 80,
-    parallelUploads: 5,
-    previewTemplate: previewTemplate,
-    autoQueue: false, // Make sure the files aren't queued until manually added
-    previewsContainer: "#previews", // Define the container to display the previews
-    clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-  });
-
-  comicUploadDropzone.on("addedfile", function(file) {
-    // Hookup the start button
-    $(file.previewElement).find('.start').on('click', function() { comicUploadDropzone.enqueueFile(file); });
-  });
-
-  // Update the total progress bar
-  comicUploadDropzone.on("totaluploadprogress", function(progress) {
-    $('#total-progress .progress-bar').css('width', progress + '%');
-  });
-
-  comicUploadDropzone.on("sending", function(file) {
-    // Show the total progress bar when upload starts
-    $('#total-progress').css('opacity', '1');
-    // And disable the start button
-    $(file.previewElement).find('.start').prop('disabled', true);
-  });
-
-  // Hide the total progress bar when nothing's uploading anymore
-  comicUploadDropzone.on("queuecomplete", function(progress) {
-    $('#total-progress').css('opacity', '0');
-  });
-
-  comicUploadDropzone.on("success", function(file, serverResponse) {
-    if (serverResponse.url) {
-      $(file.previewElement).find('.delete').attr('href', serverResponse.url);
-    } else {
-      if (serverResponse.errors) {
-        $(file.previewElement).find('.error').html(serverResponse.errors);
+    $('#select_group').on('change', function() {
+      if ($(this).val() === '') {
+        disableUploadButtons();
+      } else {
+        enableUploadButtons();
       }
+    });
+
+    // Get the template HTML and remove it from the doument
+    var previewNode = $('#template');
+    previewNode.removeAttr('id');
+    var previewTemplate = previewNode.parent().html();
+    previewNode.remove();
+
+    var comicUploadDropzone = new Dropzone('#comic_upload', {
+      paramName: 'comic',
+      thumbnailWidth: 80,
+      thumbnailHeight: 80,
+      parallelUploads: 5,
+      previewTemplate: previewTemplate,
+      autoQueue: false, // Make sure the files aren't queued until manually added
+      previewsContainer: "#previews", // Define the container to display the previews
+      clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    });
+
+    comicUploadDropzone.on("addedfile", function(file) {
+      // Hookup the start button
+      $(file.previewElement).find('.start').on('click', function() { comicUploadDropzone.enqueueFile(file); });
+    });
+
+    // Update the total progress bar
+    comicUploadDropzone.on("totaluploadprogress", function(progress) {
+      $('#total-progress .progress-bar').css('width', progress + '%');
+    });
+
+    comicUploadDropzone.on("sending", function(file) {
+      // Show the total progress bar when upload starts
+      $('#total-progress').css('opacity', '1');
+      // And disable the start button
+      $(file.previewElement).find('.start').prop('disabled', true);
+    });
+
+    // Hide the total progress bar when nothing's uploading anymore
+    comicUploadDropzone.on("queuecomplete", function(progress) {
+      $('#total-progress').css('opacity', '0');
+    });
+
+    comicUploadDropzone.on("success", function(file, serverResponse) {
+      if (serverResponse.url) {
+        $(file.previewElement).find('.delete').attr('href', serverResponse.url);
+      } else {
+        if (serverResponse.errors) {
+          $(file.previewElement).find('.error').html(serverResponse.errors);
+        }
+      }
+    });
+
+    // Setup the buttons for all transfers
+    // The "add files" button doesn't need to be setup because the config
+    // `clickable` has already been specified.
+    $('#actions .start').on('click', function(e) {
+      comicUploadDropzone.enqueueFiles(comicUploadDropzone.getFilesWithStatus(Dropzone.ADDED));
+    });
+
+    $('#actions .cancel').on('click', function() {
+      comicUploadDropzone.removeAllFiles(true);
+    });
+
+    function enableUploadButtons() {
+      $('#actions .start').prop('disabled', false);
+      $('.file-row .start').prop('disabled', false);
     }
-  });
 
-  // Setup the buttons for all transfers
-  // The "add files" button doesn't need to be setup because the config
-  // `clickable` has already been specified.
-  $('#actions .start').on('click', function(e) {
-    comicUploadDropzone.enqueueFiles(comicUploadDropzone.getFilesWithStatus(Dropzone.ADDED));
-  });
+    function disableUploadButtons() {
+      $('#actions .start').prop('disabled', true);
+      $('.file-row .start').prop('disabled', true);
+    }
 
-  $('#actions .cancel').on('click', function() {
-    comicUploadDropzone.removeAllFiles(true);
-  });
 
-  function enableUploadButtons() {
-    $('#actions .start').prop('disabled', false);
-    $('.file-row .start').prop('disabled', false);
+    // autofocus new group modal's name field.
+    $('#new-group-modal').on('shown.bs.modal', function() {
+      $('#group_name').focus();
+    });
   }
-
-  function disableUploadButtons() {
-    $('#actions .start').prop('disabled', true);
-    $('.file-row .start').prop('disabled', true);
-  }
-
-
-  // autofocus new group modal's name field.
-  $('#new-group-modal').on('shown.bs.modal', function() {
-    $('#group_name').focus();
-  });
 
 });
