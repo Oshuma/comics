efeacf451c69554908636e8735d950efcb3345cd
Dale Campbell <oshuma@gmail.com>
2017-05-03 20:50:13 -0400

    Order histories by desc.

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -3,7 +3,7 @@ class HistoryController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @histories = current_user.histories.page(params[:page])
+    @histories = current_user.histories.order(created_at: :desc).page(params[:page])
   end
 
 end
