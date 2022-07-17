8c986ab279602f28fe24b903cce968cb2a7c6ef6
Dale Campbell <oshuma@gmail.com>
2022-07-17 20:59:57 +0000

    Comic uploading

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -3,5 +3,21 @@ class ComicsController < ApplicationController
 
   def new
     @groups = current_user.groups.order(name: :asc)
+    @comic = current_user.comics.build
+  end
+
+  def create
+    @comic = current_user.comics.build(comic_params)
+    if @comic.save
+      redirect_to new_comic_path, notice: 'Comic uploaded!'
+    else
+      redirect_to new_comic_path, alert: 'There was an issue uploading that comic.'
+    end
+  end
+
+  private
+
+  def comic_params
+    params.require(:comic).permit(:archive, :group_id)
   end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -5,98 +5,98 @@ class Comic < ApplicationRecord
   belongs_to :group
   has_many :pages, -> { order(number: :asc) }, dependent: :destroy, autosave: true
 
-  def cover_image_thumb
-    # @cover_image_thumb ||= pages.first.try(:image)
-  end
+  after_create_commit :create_pages_from_archive
 
-  def upload(uploaded_file)
-    # self.filename = uploaded_file.original_filename
+  # def cover_image_thumb
+  #   # @cover_image_thumb ||= pages.first.try(:image)
+  # end
 
-    # if save
-    #   create_pages_from_archive(uploaded_file)
-    # else
-    #   false
-    # end
-  end
+  # def finished!
+  #   # pages.update_all(read: true) ? record_history! : false
+  # end
 
-  def finished!
-    # pages.update_all(read: true) ? record_history! : false
-  end
+  # def read?
+  #   # pages.all? { |page| page.read? }
+  # end
 
-  def read?
-    # pages.all? { |page| page.read? }
-  end
+  # # Returns true if there is at least one read page.
+  # def reading?
+  #   # pages.any? { |page| page.read? }
+  # end
 
-  # Returns true if there is at least one read page.
-  def reading?
-    # pages.any? { |page| page.read? }
-  end
+  # def record_history!
+  #   # user.histories.find_or_create_by!(group_name: group.name, comic_name: filename)
+  # end
 
-  def record_history!
-    # user.histories.find_or_create_by!(group_name: group.name, comic_name: filename)
-  end
+  # def move_to(group)
+  #   # self.group = group
+  #   # save
+  # end
 
-  def move_to(group)
-    # self.group = group
-    # save
-  end
-
-  def pretty_filename
-    # File.basename(filename, File.extname(filename))
-  end
+  # def pretty_filename
+  #   # File.basename(filename, File.extname(filename))
+  # end
 
   private
 
-  def create_pages_from_archive(archive_file)
-    # tempfile = archive_file.tempfile
-    # case File.extname(tempfile).downcase
-    # when '.cbr'
-    #   create_from_cbr(tempfile)
-    # when '.cbz'
-    #   create_from_cbz(tempfile)
-    # else
-    #   raise StandardError.new("Unknown file extension: #{tempfile}")
-    # end
+  def create_pages_from_archive
+    case File.extname(archive.filename.to_s).downcase
+    when '.cbr'
+      create_from_cbr
+    when '.cbz'
+      create_from_cbz
+    else
+      raise StandardError.new("Unknown file extension: #{archive.filename}")
+    end
   end
 
-  def create_from_cbr(tempfile)
-    # raise StandardError.new("unrar not found") if `which unrar`.chomp.empty?
+  def create_from_cbr
+    raise StandardError.new("unrar not found") if `which unrar`.chomp.empty?
 
-    # inside_temp_dir(tempfile) do
-    #   `unrar e #{tempfile.path}`
-    # end
+    inside_temp_dir do
+      archive.open do |f|
+        `unrar e #{f.path}`
+      end
+    end
   end
 
-  def create_from_cbz(tempfile)
-    # raise StandardError.new("unzip not found") if `which unzip`.chomp.empty?
+  def create_from_cbz
+    raise StandardError.new("unzip not found") if `which unzip`.chomp.empty?
 
-    # inside_temp_dir(tempfile) do
-    #   `unzip -j #{tempfile.path}`
-    # end
+    inside_temp_dir do
+      archive.open do |f|
+        `unzip -j #{f.path}`
+      end
+    end
   end
 
-  def inside_temp_dir(tempfile, &block)
-    # temp_dir = Rails.root.join('tmp', File.basename(tempfile.path))
-    # Dir.mkdir(temp_dir)
+  # TODO: Move this to a background job.
+  def inside_temp_dir(&block)
+    temp_dir = File.join(Dir.tmpdir, archive.filename.to_s)
+    Dir.mkdir(temp_dir)
 
-    # Dir.chdir(temp_dir) do
-    #   yield
+    Dir.chdir(temp_dir) do
+      yield
 
-    #   page_number = 1
+      page_number = 1
 
-    #   Dir['*'].sort.each do |image|
-    #     begin
-    #       pages.create!(number: page_number, image: File.open(image))
-    #       page_number += 1
-    #     # This is mainly here in case the archive has some dumbass 'Thumbs.db' file that isn't an image.
-    #     rescue => e
-    #       Rails.logger.warn "-- WARN: #{Time.now}: #{self.class}#inside_temp_dir: #{e.inspect}"
-    #       Rails.logger.warn "-- WARN: #{Time.now}: #{self.class}#inside_temp_dir: image: #{image.inspect}"
-    #       next
-    #     end
-    #   end
-    # end
+      Dir['*'].sort.each do |image|
+        begin
+          page = pages.build(number: page_number)
+          page.image.attach(io: File.open(image), filename: image)
+          page.save!
 
-    # FileUtils.rm_rf(temp_dir)
+          page.save!
+          page_number += 1
+        rescue => e
+          # This is mainly here in case the archive has some dumbass 'Thumbs.db' file that isn't an image.
+          Rails.logger.warn "-- WARN: #{Time.now}: #{self.class}#create_from_cbr: #{e.inspect}"
+          Rails.logger.warn "-- WARN: #{Time.now}: #{self.class}#create_from_cbr: image: #{image.inspect}"
+          next
+        end
+      end
+    end
+
+    FileUtils.rm_rf(temp_dir)
   end
 end
