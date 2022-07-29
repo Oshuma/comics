f071854a07d2225884ef9e40cd6e7fa0547279e6
Dale Campbell <oshuma@gmail.com>
2022-07-29 05:00:59 +0000

    Closes #29

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def flash_color_for(name)
+    case name.to_sym
+    when :alert
+      'bg-danger bg-opacity-25'
+    else
+      'bg-success bg-opacity-25'
+    end
+  end
+
+  def flash_icon_for(name)
+    case name.to_sym
+    when :alert
+      'bi bi-exclamation-triangle text-danger'
+    else
+      'bi bi-bell text-success'
+    end
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener('turbo:load', () => {
+  const toastElList = document.querySelectorAll('.toast')
+  const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, {
+    autohide: false,
+  }))
+  toastList.forEach((t) => { t.show() })
+})
